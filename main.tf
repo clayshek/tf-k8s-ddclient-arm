c6429c48cef620be2d8d1319351ee2c0f0380212
Clay Shekleton <clayshek@gmail.com>
2018-05-01 14:56:53 -0400

    Fix NFS PV Mount

--- main.tf
+++ main.tf
@@ -30,55 +30,62 @@ resource "kubernetes_config_map" "ddclient-config" {
   }
 
   data {
-    ddclient_hostname = "${var.ddclient_hostname}"
-    ddclient_server = "${var.ddclient_server}"
-    ddclient_protocol = "${var.ddclient_protocol}"
+    ddclient_hostname        = "${var.ddclient_hostname}"
+    ddclient_server          = "${var.ddclient_server}"
+    ddclient_protocol        = "${var.ddclient_protocol}"
     ddclient_daemon_interval = "${var.ddclient_daemon_interval}"
   }
 }
-
 
 # PERSISTENT VOLUME
 # Coded here for NFS, but can be modified for other supported K8s persistent volume types.
 # See https://www.terraform.io/docs/providers/kubernetes/r/persistent_volume.html
 
-
 resource "kubernetes_persistent_volume" "ddclient_pv" {
-    metadata {
-        name = "ddclient-pv"
+  metadata {
+    name = "ddclient-pv"
+  }
+
+  spec {
+    capacity {
+      storage = "${var.storage_capacity}"
     }
-    spec {
-        capacity {
-            storage = "${var.storage_capacity}"
-        }
-        access_modes = ["ReadWriteMany"]
-        storage_class_name = "ddclient"
-        persistent_volume_source {
-            nfs {
-                path = "${var.vol_path}"
-                server = "${var.nfs_server}"
-            }
-        }
+
+    access_modes                     = ["ReadWriteMany"]
+    persistent_volume_reclaim_policy = "Retain"
+    storage_class_name               = "ddclient"
+
+    persistent_volume_source {
+      nfs {
+        path      = "${var.vol_path}"
+        server    = "${var.nfs_server}"
+        read_only = "false"
+      }
     }
+  }
 }
 
 # PERSISTENT VOLUME CLAIM
 
 resource "kubernetes_persistent_volume_claim" "ddclient-pv-claim" {
   metadata {
-    name = "ddclient-pv-claim"
+    name = "ddclient-pvc"
+
     labels {
       app = "ddclient"
     }
   }
+
   spec {
     access_modes = ["ReadWriteMany"]
+
     resources {
       requests {
         storage = "${var.storage_capacity}"
       }
     }
-    volume_name = "ddclient-pv"
+
+    volume_name        = "ddclient-pv"
     storage_class_name = "ddclient"
   }
 }
@@ -90,6 +97,7 @@ resource "kubernetes_persistent_volume_claim" "ddclient-pv-claim" {
 resource "kubernetes_replication_controller" "ddclient" {
   metadata {
     name = "ddclient"
+
     labels {
       app = "ddclient"
     }
@@ -99,7 +107,9 @@ resource "kubernetes_replication_controller" "ddclient" {
     selector {
       app = "ddclient"
     }
+
     replicas = 1
+
     template {
       container {
         image = "${var.image_name}"
@@ -107,19 +117,22 @@ resource "kubernetes_replication_controller" "ddclient" {
 
         env {
           name = "DDNS_USERNAME"
+
           value_from {
             secret_key_ref {
               name = "ddclient-auth"
-              key = "username"
+              key  = "username"
             }
           }
         }
+
         env {
           name = "DDNS_PASSWORD"
+
           value_from {
             secret_key_ref {
               name = "ddclient-auth"
-              key = "password"
+              key  = "password"
             }
           }
         }
@@ -128,54 +141,61 @@ resource "kubernetes_replication_controller" "ddclient" {
         # See https://github.com/terraform-providers/terraform-provider-kubernetes/issues/78
         env {
           name = "DDNS_HOSTNAME"
+
           value_from {
             config_map_key_ref {
               name = "ddclient-config"
-              key = "ddclient_hostname"
+              key  = "ddclient_hostname"
             }
           }
         }
+
         env {
           name = "DDNS_SERVER"
+
           value_from {
             config_map_key_ref {
               name = "ddclient-config"
-              key = "ddclient_server"
-            }
-          }
-        }        
-        env {
-          name = "DDNS_PROTOCOL"
-          value_from {
-            config_map_key_ref {
-              name = "ddclient-config"
-              key = "ddclient_protocol"
+              key  = "ddclient_server"
             }
           }
         }
+
         env {
-          name = "DDNS_DAEMON_INTERVAL"
+          name = "DDNS_PROTOCOL"
+
           value_from {
             config_map_key_ref {
               name = "ddclient-config"
-              key = "ddclient_daemon_interval"
+              key  = "ddclient_protocol"
+            }
+          }
+        }
+
+        env {
+          name = "DDNS_DAEMON_INTERVAL"
+
+          value_from {
+            config_map_key_ref {
+              name = "ddclient-config"
+              key  = "ddclient_daemon_interval"
             }
           }
         }
 
         volume_mount {
-          name = "ddclient-pvc"
+          name       = "ddclient"
           mount_path = "/var/cache/ddclient"
         }
       }
 
       volume {
-        name = "ddclient-pvc"
+        name = "ddclient"
+
         persistent_volume_claim {
-          claim_name = "ddclient-pv-claim"
+          claim_name = "ddclient-pvc"
         }
       }
-
     }
   }
 }
